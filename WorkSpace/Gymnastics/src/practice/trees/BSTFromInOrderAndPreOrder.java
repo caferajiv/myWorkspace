b7444055b80200663a1941b7d9b4a6feb47ed589
code-Phreak <crazyrajiv@gmail.com>
2017-08-26 22:08:31 +0530

    tree

--- WorkSpace/Gymnastics/src/practice/trees/BSTFromInOrderAndPreOrder.java
+++ WorkSpace/Gymnastics/src/practice/trees/BSTFromInOrderAndPreOrder.java
@@ -1,5 +1,47 @@
 package practice.trees;
 
-public class BinaryTreeFromInOrderAndPreOrder {
+import node.TreeNode;
+import tree.BinaryTree;
+
+public class BSTFromInOrderAndPreOrder {
+	
+	public static int  preIndex = 0;
+
+	public static void main(String[] args) {
+
+		int[] inOrder = { 4, 2, 5, 1, 6, 3 };
+		int[] preOrder = { 1, 2, 4, 5, 3, 6 };
+
+		BinaryTree bTree = new BinaryTree();
+		bTree.setRoot(createBTNode(inOrder, preOrder, 0, inOrder.length - 1));
+		bTree.InorderTraversal(bTree.getRoot());
+
+	}
+
+	private static TreeNode createBTNode(int[] inOrder, int[] preOrder, int min, int max) {
+		if (min < 0 || max >= inOrder.length || min > max)
+			return null;
+		TreeNode node = new TreeNode(preOrder[preIndex]);
+		if (preIndex < preOrder.length - 1) {
+			int index = findPreIndexinInorder(inOrder, preOrder, preIndex, min, max);
+			if (index != -1) {
+				preIndex++;
+				node.setLeftChild(createBTNode(inOrder, preOrder, min, index - 1));
+
+				node.setRightChild(createBTNode(inOrder, preOrder, index + 1, max));
+			}
+		}
+
+		return node;
+	}
+
+	private static int findPreIndexinInorder(int[] inOrder, int[] preOrder, int preIndex, int min, int max) {
+		for (int i = min; i <= max; i++) {
+			if (preOrder[preIndex] == inOrder[i]) {
+				return i;
+			}
+		}
+		return -1;
+	}
 
 }
