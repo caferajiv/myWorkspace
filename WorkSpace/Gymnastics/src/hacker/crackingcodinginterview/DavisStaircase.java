53b325d57c1b42eef7943e4ea0d360396255f7d6
code-Phreak <crazyrajiv@gmail.com>
2017-10-02 13:54:31 +0530

    Updated davis staircase solution

--- WorkSpace/Gymnastics/src/hacker/crackingcodinginterview/DavisStaircase.java
+++ WorkSpace/Gymnastics/src/hacker/crackingcodinginterview/DavisStaircase.java
@@ -23,17 +23,22 @@ public class DavisStaircase {
 	
 	
 	private static int findStepsDP(int n) {
-		int arr[] = new int[100];
+		int arr[] = new int[n + 1];
 		arr[0] = 0;
-		arr[1] = 1;
-		arr[2] = 2;
-		arr[3] = 4;
-		
-		for(int i=4;i<=n;i++) {
-			arr[i]= arr[i-1]+arr[i-2]+arr[i-3];
+		if (n >= 1) {
+			arr[1] = 1;
 		}
-		
-		return arr[n];
-	}
+		if (n >= 2) {
+			arr[2] = 2;
+		}
+		if (n >= 3) {
+			arr[3] = 4;
+		}
+
+		for (int i = 4; i <= n; i++) {
+			arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3];
+		}
+
+		return arr[n];}
 }
 
