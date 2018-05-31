b473d24c5202df32811eec4559bfa40bfedc22b7
code-Phreak <crazyrajiv@gmail.com>
2018-06-01 03:10:37 +0530

    graph

--- WorkSpace/Data/src/com/data/graph/Graph.java
+++ WorkSpace/Data/src/com/data/graph/Graph.java
@@ -8,7 +8,7 @@ import java.util.Map;
 import java.util.Set;
 
 public abstract class Graph {
-	protected Map<Node, Set<Edge>> adjacencySets;
+	protected Map<Node, Set<Edge>> adjacencyMap;
 	protected int numNodes;
 	protected int numEdges;
 	protected Map<String, Node> elementToNode;
@@ -20,7 +20,7 @@ public abstract class Graph {
 	public abstract boolean removeEdge(Node node1, Node node2);
 
 	public Graph() {
-		adjacencySets = new HashMap<Node, Set<Edge>>();
+		adjacencyMap = new HashMap<Node, Set<Edge>>();
 		elementToNode = new HashMap<String, Node>();
 		numNodes = 0;
 		numEdges = 0;
@@ -32,7 +32,7 @@ public abstract class Graph {
 			return false;
 		}
 		Set<Edge> newAdjacencySet = new HashSet<Edge>();
-		adjacencySets.put(newNode, newAdjacencySet);
+		adjacencyMap.put(newNode, newAdjacencySet);
 		elementToNode.put(newNode.getElement(), newNode);
 		numNodes++;
 		return true;
@@ -42,7 +42,7 @@ public abstract class Graph {
 		if (!containsNode(node)) {
 			return null;
 		}
-		Set<Edge> nodeEdges = adjacencySets.get(node);
+		Set<Edge> nodeEdges = adjacencyMap.get(node);
 		Set<Node> nodeNeighbors= new HashSet<Node>();
 		for (Edge e : nodeEdges) {
 			Node neighbor = e.getDestination();
@@ -55,7 +55,7 @@ public abstract class Graph {
 	protected boolean addEdgeFromTo(Node source,
 			Node destination) {
 		Edge newEdge = new Edge(source, destination);
-		Set<Edge> sourceEdges = adjacencySets.get(source);
+		Set<Edge> sourceEdges = adjacencyMap.get(source);
 		if (!sourceEdges.contains(newEdge)) {
 			sourceEdges.add(newEdge);
 			return true;
@@ -66,7 +66,7 @@ public abstract class Graph {
 	protected boolean removeEdgeFromTo(Node source,
 			Node destination) {
 		Edge toRemove = new Edge(source, destination);
-		Set<Edge> sourceEdges = adjacencySets.get(source);
+		Set<Edge> sourceEdges = adjacencyMap.get(source);
 		if (sourceEdges.contains(toRemove)) {
 			sourceEdges.remove(toRemove);
 			return true;
@@ -84,7 +84,7 @@ public abstract class Graph {
 	}
 	
 	public Node getStartingNode() {
-		Iterator<Node> iter = adjacencySets.keySet().iterator();
+		Iterator<Node> iter = adjacencyMap.keySet().iterator();
 		if (iter.hasNext()) {
 			return iter.next();
 		}
@@ -92,18 +92,18 @@ public abstract class Graph {
 	}
 	
 	public Set<Node> getAllNodes() {
-		return adjacencySets.keySet();
+		return adjacencyMap.keySet();
 	}
 	
 	public Set<Edge> getNodeEdges(Node node) {
 		if (!containsNode(node)) {
 			return null;
 		}
-		return adjacencySets.get(node);
+		return adjacencyMap.get(node);
 	}
 	
 	public boolean containsNode(Node node) {
-		return adjacencySets.containsKey(node);
+		return adjacencyMap.containsKey(node);
 	}
 	
 	public Node getNode(String element) {
